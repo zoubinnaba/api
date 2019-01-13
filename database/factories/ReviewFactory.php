006136de3f1a84dd1c15a1b001236207c0a552ee
Pangraf <emmanuelzoubinnaba@gmail.com>
2019-01-13 11:55:07 +0000

    Seed de mes models Product et Review

--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -1,9 +1,20 @@
 <?php
 
+use App\Model\Product;
 use Faker\Generator as Faker;
 
 $factory->define(App\Model\Review::class, function (Faker $faker) {
     return [
-        //
+        'product_id' => function(){
+
+        	return Product::all()->random();
+        },
+
+        'custumer' => $faker->name,
+
+        'review' => $faker->paragraph,
+
+        'star' => $faker->numberBetween(0,5)
+
     ];
 });
--- database/factories/RewiewFactory.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Faker\Generator as Faker;
-
-$factory->define(App\Model\Rewiew::class, function (Faker $faker) {
-    return [
-        //
-    ];
-});
